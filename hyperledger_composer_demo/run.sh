0e83b26a3f49c837b9cd86109ec8d855e32ff970
nkapsoulis <39044618+nkapsoulis@users.noreply.github.com>
2018-07-19 11:54:17 +0300

    Update run.sh

--- hyperledger_composer_demo/run.sh
+++ hyperledger_composer_demo/run.sh
@@ -7,4 +7,4 @@ cd ../news-composer/
 composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName news-network
 composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile news-network@1.1.0.bna --file admin@news-network
 composer card import --file admin@news-network
-composer-rest-server -c admin@news-network -n never -w true
+composer-rest-server -c admin@news-network -n never -w true &
