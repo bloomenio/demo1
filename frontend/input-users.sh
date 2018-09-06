574fe592f0f6def09628fd9d4ae134f6435a38d8
Nikos Kapsoulis <nkapsoulis@gmail.com>
2018-09-06 14:49:37 +0300

    input users file

--- frontend/input-users.sh
+++ frontend/input-users.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \ 
-   "$class": "org.acme.sample.User", \ 
-   "userId": "1", \ 
-   "firstName": "Larry", \ 
-   "lastName": "Brown", \ 
-   "balance": 10, \ 
-   "reputation": 2 \ 
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+   "$class": "org.acme.sample.User",
+   "userId": "1",
+   "firstName": "Larry",
+   "lastName": "Brown",
+   "balance": 10,
+   "reputation": 2
  }' 'http://demo1.bloomen.io:3000/api/User'
 
- curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \ 
-   "$class": "org.acme.sample.User", \ 
-   "userId": "2", \ 
-   "firstName": "Cameron", \ 
-   "lastName": "Fox", \ 
-   "balance": 20, \ 
-   "reputation": 3 \ 
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+   "$class": "org.acme.sample.User",
+   "userId": "2",
+   "firstName": "Cameron",
+   "lastName": "Fox",
+   "balance": 20,
+   "reputation": 3
  }' 'http://demo1.bloomen.io:3000/api/User'
+
 
